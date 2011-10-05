2ebdbc9729ba24366be419bd8dee313fb8981ba6
unknown <tmacblane@.us.corp.mimeo.com>
2011-10-05 11:27:47 -0400

    added if statement for sending email notification

--- PlayerBounties/Helpers/EmailNotificationHelper.cs
+++ PlayerBounties/Helpers/EmailNotificationHelper.cs
@@ -19,31 +19,34 @@ namespace PlayerBounties.Helpers
 
 		public void SendNotificationEmail(Bounty bounty, string emailView, Guid accountId)
 		{
-			dynamic email = new Email(emailView);
-
-			string placedOn = bounty.CharacterName(bounty.PlacedOnId);
-			string placedBy = bounty.CharacterName(bounty.PlacedById);
-			string killedBy = string.Empty;
-
-			email.UserEmailAddress = this.db.Accounts.Find(accountId).EmailAddress;
-			email.Amount = bounty.Amount;
-			email.Reason = bounty.Reason;
-			email.Message = bounty.Message;
-			email.ClientName = placedBy;
-			email.TargetName = placedOn;
-			
-			if(bounty.KilledById != null)
+			if(this.db.Accounts.Find(accountId).EmailNotification == true)
 			{
-				killedBy = bounty.CharacterName(bounty.KilledById.Value);
-			}
+				dynamic email = new Email(emailView);
 
-			try
-			{
-				email.Send();
-			}
-			catch
-			{
-				// Need to log when it fails, the email type and information
+				string placedOn = bounty.CharacterName(bounty.PlacedOnId);
+				string placedBy = bounty.CharacterName(bounty.PlacedById);
+				string killedBy = string.Empty;
+
+				email.UserEmailAddress = this.db.Accounts.Find(accountId).EmailAddress;
+				email.Amount = bounty.Amount;
+				email.Reason = bounty.Reason;
+				email.Message = bounty.Message;
+				email.ClientName = placedBy;
+				email.TargetName = placedOn;
+
+				if(bounty.KilledById != null)
+				{
+					killedBy = bounty.CharacterName(bounty.KilledById.Value);
+				}
+
+				try
+				{
+					email.Send();
+				}
+				catch
+				{
+					// Need to log when it fails, the email type and information
+				}
 			}
 		}
 
